29d38047f1a9ed2665a80e08a173469b5c6e44af
QiMeng <616684889@qq.com>
2014-06-19 09:33:46 +0800

    hello world

--- js/controllers.js
+++ js/controllers.js
@@ -5,12 +5,12 @@ angular.module('starter.controllers', [])
 
 .controller('PlaylistsCtrl', function($scope) {
   $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
+    { title: '1.Hello World', id: 1 },
+    { title: '2', id: 2 },
+    { title: '3', id: 3 },
+    { title: '4', id: 4 },
+    { title: '5', id: 5 },
+    { title: '6', id: 6 }
   ];
 })
 
